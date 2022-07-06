e0d1e6b24993b03be8e510922df392c10e294faa
HARSHAVARDHAN <HARSHAVARDHAN@LAPTOP-8ADG3AEO>
2022-07-06 09:15:16 +0530

    oriject commit

---
+++ ShanonCRM/src/com/Shanon/GenericLib/IAutoConsts.java
@@ -0,0 +1,15 @@
+package com.Shanon.GenericLib;
+
+public interface IAutoConsts {
+
+	
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="C:\\Users\\HARSHAVARDHAN\\Downloads\\ShanonCRM\\drivers\\chromedriver.exe";
+	
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="C:\\Users\\HARSHAVARDHAN\\Downloads\\ShanonCRM\\drivers\\geckodriver.exe";
+	
+	String EXCEL_PATH="C:\\Users\\HARSHAVARDHAN\\Downloads\\ShanonCRM\\data\\inputData.xlsx";
+	String PROP_PATH="C:\\Users\\HARSHAVARDHAN\\Downloads\\ShanonCRM\\data\\config.properties";
+	
+}
